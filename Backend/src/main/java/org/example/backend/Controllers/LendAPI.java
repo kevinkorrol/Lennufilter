17cd64ba72bf9ed7973d0bb7405e2e4c9238ed3c
Kevin Markus Korrol <kevinmak@ut.ee>
2025-03-27 23:37:18 +0200

    api fix

--- Backend/src/main/java/org/example/backend/Controllers/LendAPI.java
+++ Backend/src/main/java/org/example/backend/Controllers/LendAPI.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "api/lend")
+@CrossOrigin(origins = "http://localhost:3000")
 public class LendAPI {
     private LendService lendService;
 
