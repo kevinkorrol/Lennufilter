6594a5f590e41afa99fe616044f9e2309ef7a186
Kevin Markus Korrol <kevinmak@ut.ee>
2025-03-28 14:48:56 +0200

    muudatused

--- Backend/src/main/java/org/example/backend/conf/IsteConf.java
+++ Backend/src/main/java/org/example/backend/conf/IsteConf.java
@@ -16,7 +16,7 @@ public class IsteConf {
 
 
     private static List<String> koikKohad(){
-        int ridadeArv = 30;
+        int ridadeArv = 15;
         List<String> kohaTahed = Arrays.asList("A", "B", "C", "D", "E", "F");
 
         List<String> rtr = new ArrayList<>();
@@ -52,7 +52,7 @@ public class IsteConf {
 
 
             for (Lend lend : lennud){
-                List<String> kinni = kinnisedKohad(70);
+                List<String> kinni = kinnisedKohad(25);
 
                 for(String koht : koikKohad){
 
@@ -67,7 +67,7 @@ public class IsteConf {
             }
             //A, F - akna all
             //C, D - rohkem ruumi
-            //Read 1 kuni 5, a-5 kuni a - lähedal väljapääsule
+            //Read 1 kuni 5, a-4 kuni a - lähedal väljapääsule
             //Vb pole vaja teha eraldi istmetüüpi, vaid saan kuskil hiljem
             // filtreerida vastaval oma ärireeglitele
         };
